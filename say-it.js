730645afa42f53d1298b7ad613fa2b7e04337144
Junv Zhao <wahyd4@gmail.com>
2017-06-08 16:27:17 +0800

    Finish the functionality to read the texts

--- say-it.js
+++ say-it.js
@@ -3,14 +3,15 @@ const program = require('commander')
 const request = require('request')
 const fs = require('fs')
 const URL = require('url').URL
-const player = require('play-sound')(opts = {players:['afplay','mplayer', 'play']})
+const player = require('play-sound')(opts = { players: ['afplay', 'mplayer', 'play'] })
+const charm = require('charm')()
+const { TEXT_ERROR, LOADING }  = require('./texts')
 
-const errorMessage = "请确保输入文本正确。 如: 'hello world' 或 \" hello world \" "
-
-
-const showError = (error = errorMessage) => {
+const showError = (error = TEXT_ERROR) => {
   console.log(chalk.red(error))
 }
+
+charm.pipe(process.stdout);
 
 program
   .version('0.0.1')
@@ -20,32 +21,10 @@ program
 program.on('*', () => {
   const text = process.argv[2]
   if (!text || text.trim() === '') {
-    showError()
-    return
+    return showError()
   }
 
-
-  const url = `http://tsn.baidu.com/text2audio?tex=${text}&lan=zh&cuid=${new Date().getTime()}&ctp=1&tok=24.9d61601aef23f1d3497c9c40eb30e7a7.2592000.1499416588.282335-9739014&per=0`
-  console.log(new URL(url).toString())
-  request
-    .get(new URL(url).toString())
-    .on('response', function (response) {
-
-      if ((response.statusCode !== 200) || (response.headers['content-type'] !== 'audio/mp3')) {
-        showError()
-        return
-      }
-    })
-    .pipe(fs.createWriteStream('say-it.mp3'))
-    // .pipe(() => {
-    //   console.log('----')
-    //   // player.play('say-it.mp3', function (err) {
-    //   //   if (err) throw err
-    //   // })
-    // })
-  player.play('/Users/junv/code/say-it/say-it.mp3', function (err) {
-    showError(err)
-  })
+  sayIt(text)
 })
 
 program.parse(process.argv)
@@ -54,4 +33,26 @@ if (!process.argv.slice(2).length) {
   program.outputHelp((text) => {
     console.log(chalk.blue(text))
   })
+}
+
+function sayIt(text) {
+  const url = `http://tsn.baidu.com/text2audio?tex=${text}&lan=zh&cuid=${new Date().getTime()}&ctp=1&tok=24.9d61601aef23f1d3497c9c40eb30e7a7.2592000.1499416588.282335-9739014&per=0`
+
+  charm.write(chalk.bgCyan(LOADING))
+  request
+    .get(new URL(url).toString())
+    .on('response', function (response) {
+      if ((response.statusCode !== 200) || (response.headers['content-type'] !== 'audio/mp3')) {
+        showError()
+        return
+      }
+    })
+    .pipe(fs.createWriteStream('say-it.mp3'))
+    .on('finish', () => {
+      player.play('say-it.mp3', function (err) {
+        showError(err)
+      })
+      charm.erase('start')
+      charm.left(LOADING.length)
+    })
 }
---
+++ texts.js
@@ -0,0 +1,7 @@
+const TEXT_ERROR = "请确保输入文本正确。 如: 'hello world' 或 \" hello world \" "
+const LOADING = 'loading...'
+
+module.exports = {
+  TEXT_ERROR,
+  LOADING
+}
