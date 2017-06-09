a91a0bb74329453b33f2f1ddc6ad45e612697e85
Junv Zhao <wahyd4@gmail.com>
2017-06-09 10:59:26 +0800

    #2 Add ability to allow user choose different person.

--- say-it.js
+++ say-it.js
@@ -4,27 +4,37 @@ const chalk = require('chalk')
 const program = require('commander')
 const request = require('request')
 const fs = require('fs')
-const player = require('play-sound')(opts = { players: ['afplay', 'mplayer', 'play', 'mpg123', 'mpg321', 'aplay', 'cmdmp3'] })
+const player = require('play-sound')(opts = {
+  players: ['afplay', 'mplayer', 'play', 'mpg123', 'mpg321', 'aplay', 'cmdmp3']
+})
 const charm = require('charm')()
-const { TEXT_ERROR, LOADING } = require('./texts')
+const TEXTS = require('./texts')
+const _ = require('lodash')
 
-const showError = (error = TEXT_ERROR) => {
+const showError = (error = TEXTS.error) => {
   console.log(chalk.red(error))
 }
+
+var person = 0
 
 charm.pipe(process.stdout);
 
 program
   .version('0.0.1')
-  .command('', 'read the texts you typed in', { isDefault: true })
-  .option('-p, --person [person]', 'choose the voice')
+  .command(' ', 'read the texts you typed in', {
+    isDefault: true
+  })
+  .option('-p, --person [person]', "set different voice. 0: female, 1 and 2: male, 3: male with emotion, 4: female with emotion")
 
 program.on('*', () => {
-  const text = process.argv[2]
+  const text = program.args[0]
   if (!text || text.trim() === '') {
     return showError()
   }
-
+  if (!_.includes([0, 1, 2, 3, 4], Number.parseInt(program.person))) {
+    return showError(TEXTS.invalidPerson)
+  }
+  person = Number.parseInt(program.person)
   sayIt(text)
 })
 
@@ -37,13 +47,12 @@ if (!process.argv.slice(2).length) {
 }
 
 function sayIt(text) {
-  const urlAddress = `http://tsn.baidu.com/text2audio?tex=${text}&lan=zh&cuid=${new Date().getTime()}&ctp=1&tok=24.9d61601aef23f1d3497c9c40eb30e7a7.2592000.1499416588.282335-9739014&per=0`
-  charm.write(chalk.bgCyan(LOADING))
+  const urlAddress = `http://tsn.baidu.com/text2audio?tex=${text}&lan=zh&cuid=${new Date().getTime()}&ctp=1&tok=24.9d61601aef23f1d3497c9c40eb30e7a7.2592000.1499416588.282335-9739014&per=${person}`
+  charm.write(chalk.bgCyan(TEXTS.loading))
   request
     .get(encodeURI(urlAddress))
     .on('response', function (response) {
       if ((response.statusCode !== 200) || (response.headers['content-type'] !== 'audio/mp3')) {
-        console.log(response.headers['content-type'])
         showError()
         return
       }
@@ -56,7 +65,7 @@ function sayIt(text) {
         }
       })
       charm.erase('line')
-      charm.left(LOADING.length)
+      charm.left(TEXTS.loading.length)
       charm.destroy()
     })
 }
--- texts.js
+++ texts.js
@@ -1,7 +1,7 @@
-const TEXT_ERROR = "请确保输入文本正确。 如: 'hello world' 或 \" hello world \" "
-const LOADING = 'loading...'
-
-module.exports = {
-  TEXT_ERROR,
-  LOADING
+const TEXTS = {
+  error: "请确保输入文本正确。 如: 'hello world' 或 \" hello world \" ",
+  loading: 'loading...',
+  invalidPerson: 'Invalid person option'
 }
+
+module.exports = TEXTS
