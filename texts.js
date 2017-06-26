fe6f0ac57b674a6281c951f947b2b328228dd483
Junv Zhao <wahyd4@gmail.com>
2017-06-26 17:25:57 +0800

    Node.js code deleted,

--- texts.js
+++
@@ -1,8 +0,0 @@
-const TEXTS = {
-  error: "请确保输入文本正确。 如: 'hello world' 或 \" hello world \" ",
-  loading: 'loading...',
-  invalidPerson: 'Invalid person option',
-  errorOption: '请输入正确的选项值'
-}
-
-module.exports = TEXTS
