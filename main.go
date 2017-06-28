3497c96a7c8d4411eaed252816f455858cb28bb9
Junv Zhao <wahyd4@gmail.com>
2017-06-28 12:08:17 +0800

    try to fix the pronounce issue under winddows

--- main.go
+++ main.go
@@ -163,7 +163,7 @@ func speak() {
 		}
 		return
 	}
-	command := exec.Command("./cmdmp3.exe", getAudioFilePath())
+	command := exec.Command("cmdmp3.exe", getAudioFilePath())
 	if err := command.Run(); err != nil {
 		log.Error("Failed to say the words: " + err.Error())
 	}
